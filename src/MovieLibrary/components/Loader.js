f89861e86f7091e39ad1da384ae15676f4c43970
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-10-09 12:58:14 -0300

    TASK 3 - Create Loader

---
+++ src/MovieLibrary/components/Loader.js
@@ -0,0 +1,20 @@
+import { Box, Text } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/react";
+import React from "react";
+
+const Loader = () => {
+  return (
+    <Box textAlign="center" my="8rem">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="red.500"
+        size="xl"
+      />
+      <Text>Loading...</Text>
+    </Box>
+  );
+};
+
+export default Loader;
