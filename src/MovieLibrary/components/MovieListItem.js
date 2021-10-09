e34f7ecf40d0ba78050dcd3418854c46d1e81a34
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-10-09 13:09:15 -0300

    Add ranking information over each movie card

--- src/MovieLibrary/components/MovieListItem.js
+++ src/MovieLibrary/components/MovieListItem.js
@@ -2,15 +2,37 @@ import React from "react";
 import { Img } from "@chakra-ui/image";
 import { Container } from "@chakra-ui/layout";
 import Pulse from "react-reveal/Pulse";
+import { CircularProgress, CircularProgressLabel, Box } from "@chakra-ui/react";
 
 const MovieListItem = ({ movie, showMovie, onOpen }) => {
+  console.log(movie.vote_average * 10);
+
   return (
     <Pulse>
       <Container onClick={onOpen}>
+        <Box
+          onClick="disable"
+          textAlign="right"
+          mr="-1rem"
+          position="relative"
+          bottom="-1.5rem"
+        >
+          <CircularProgress
+            value={movie.vote_average * 10}
+            color="green"
+            bg="white"
+            borderRadius="full"
+          >
+            <CircularProgressLabel color="black" fontWeight="bold">
+              {movie.vote_average * 10}%
+            </CircularProgressLabel>
+          </CircularProgress>
+        </Box>
+
         <Img
           src={`${"https://image.tmdb.org/t/p/w500/"}${movie.poster_path}`}
           alt={movie.title}
-          w="10rem"
+          w={["15rem", "10rem"]}
           borderRadius="1rem"
           webkittransform=""
           boxShadow="2xl"
